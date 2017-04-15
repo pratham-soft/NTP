20407b3552b2c2ed55309389baf03abae1cd6a23
AtulAttri <attri987@gmail.com>
2017-04-15 13:31:46 +0530

    File Upload done in leads page only

--- app/controllers/leads.js
+++ app/controllers/leads.js
@@ -1,6 +1,56 @@
 app.controller("leads", function($scope, $http, $cookieStore, $uibModal, $state, $window) {
     $scope.searchLead = ''; // set the default search/filter term
     $scope.selected = []; //stores checked items only
+    
+            // GET THE FILE INFORMATION.
+        $scope.getFileDetails = function (e) {
+
+            $scope.files = [];
+            $scope.$apply(function () {
+
+                // STORE THE FILE OBJECT IN AN ARRAY.
+                for (var i = 0; i < e.files.length; i++) {
+                    $scope.files.push(e.files[i])
+                }
+
+            });
+        };
+
+        // NOW UPLOAD THE FILES.
+        $scope.uploadFiles = function () {
+
+            //FILL FormData WITH FILE DETAILS.
+            var data = new FormData();
+
+            for (var i in $scope.files) {
+                data.append("uploadedFile", $scope.files[i]);
+            }
+
+            // ADD LISTENERS.
+            var objXhr = new XMLHttpRequest();
+            objXhr.addEventListener("progress", updateProgress, false);
+            objXhr.addEventListener("load", transferComplete, false);
+
+            // SEND FILE DETAILS TO THE API.
+            objXhr.open("POST", "http://120.138.8.150/pratham/Test/fileupload");
+           
+          
+            objXhr.send(data);
+        }
+
+        // UPDATE PROGRESS BAR.
+        function updateProgress(e) {
+            if (e.lengthComputable) {
+                document.getElementById('pro').setAttribute('value', e.loaded);
+                document.getElementById('pro').setAttribute('max', e.total);
+            }
+        }
+
+        // CONFIRMATION.
+        function transferComplete(e) {
+            alert("Files uploaded successfully.");
+        }
+    
 
     ($scope.getLeads = function() {
         angular.element(".loader").show();
