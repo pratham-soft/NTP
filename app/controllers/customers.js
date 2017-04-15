2b05e61817c3ee13c9ddcd5f8a45274cade1575f
divu <diwakar.rao@gmail.com>
2017-04-15 11:26:48 +0530

    F Commit

---
+++ app/controllers/customers.js
@@ -0,0 +1,110 @@
+app.controller("customerController", function($scope, $http, $cookieStore, $state, $uibModal) {
+    ($scope.getCustomers = function() {
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/User/UserDtls/ByUserType",
+            ContentType: 'application/json',
+            data: {
+                "user_comp_guid": $cookieStore.get('comp_guid'),
+                "user_type": 4
+            }
+        }).success(function(data) {
+            //console.log(data);
+            angular.element(".loader").hide();
+            $scope.customers = data;
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    })();
+
+    $scope.customerDetail = function(selectedItem) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'customerDetail.html',
+            controller: 'customerDetailController',
+            size: 'lg',
+            backdrop: 'static',
+            resolve: {
+                item: function() {
+                    return $scope.customers[selectedItem];
+                }
+            }
+        });
+    };
+});
+
+app.controller("customerDetailController", function($scope, $http, $cookieStore, $state, $uibModalInstance, item) {
+    $scope.customer = item;
+    $scope.unitStatus = [];
+    $scope.unitStatus[2] = "Interested";
+    $scope.unitStatus[4] = "Blocked by paying advance";
+    $scope.unitStatus[5] = "Blocked by not paying advance";
+    $scope.unitStatus[6] = "Sold";
+    $scope.unitStatus[7] = "Cancelled";
+
+    $scope.leadId = $scope.customer.user_id;
+
+    if ($scope.customer.userprojlist != null) {
+        $scope.leadProjects = [];
+        for (i = 0; i < $scope.customer.userprojlist.length; i++) {
+            $scope.leadUnitObj = $scope.customer.userprojlist[i];
+            $scope.leadUnitObj.unitViewStatus = "N/A";
+            if ($scope.customer.userprojlist[i].ProjDtl_Status != 0)
+                $scope.leadUnitObj.unitViewStatus = $scope.unitStatus[$scope.customer.userprojlist[i].ProjDtl_Status];
+            $scope.leadProjects.push($scope.leadUnitObj);
+        }
+    }
+
+    $scope.deleteRow = function(projId, rowId) {
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/User/ProjUnitDel",
+            ContentType: 'application/json',
+            data: [{
+                "comp_guid": $cookieStore.get('comp_guid'),
+                "ProjDtl_Id": projId
+            }]
+        }).success(function(data) {
+            if (data.Comm_ErrorDesc == '0|0') {
+                $("tr#" + rowId).remove();
+                $("#unit" + rowId).removeClass('selected');
+            }
+            angular.element(".loader").hide();
+        }).error(function() {
+            alert('Something went wrong.');
+            angular.element(".loader").hide();
+        });
+    };
+
+    $scope.addLeadProjects = function(leadId) {
+        $uibModalInstance.close();
+        $state.go("/ProjectDetails", {
+            "leadID": $scope.leadId
+        });
+    };
+
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+
+    $scope.getTypeNameById = function(typeId) {
+        var typeName = '';
+        switch (parseInt(typeId)) {
+            case 1:
+                typeName = 'Flat';
+                break;
+            case 2:
+                typeName = 'Sites';
+                break;
+            case 3:
+                typeName = 'Villa';
+                break;
+            case 4:
+                typeName = 'Row Houses';
+                break;
+            default:
+                console.log('eror');
+        }
+        return typeName;
+    }
+});
---
+++ app/controllers/unit-listing.js
@@ -0,0 +1,135 @@
+app.controller("unitsListingController", function($scope, $http, $cookieStore, $state, $uibModal) {
+    $scope.unitStatus = ['vacant', 'userinterest', 'mgmtquota', 'blockedbyadvnc', 'blockedbynotadvnc', 'sold'];
+    $scope.unitStatusText = ['Vacant', 'User Interested', 'Management Quota', 'Blocked By Paying Advance', 'Blocked By Not Paying Advance', 'Sold'];
+
+    ($scope.getProjectList = function() {
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Proj/ProjDtls/ByCompGuid",
+            ContentType: 'application/json',
+            data: {
+                "Proj_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+            $scope.projectList = data;
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    })();
+
+    $scope.getPhaseList = function(projectName) {
+        $scope.flatType = "";
+        $scope.projectDetails.phase = "";
+        $scope.projectDetails.blocks = "";
+        $scope.blockList = {};
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Proj/PhaseDtls/ByPhaseProjId",
+            ContentType: 'application/json',
+            data: {
+                "Phase_Proj_Id": projectName,
+                "Phase_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+            $scope.phaseList = data;
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    };
+    $scope.getBlockList = function(phase, projectName) {
+        $scope.projectDetails.blocks = "";
+        for (i = 0; i < $scope.phaseList.length; i++) {
+            if ($scope.phaseList[i].Phase_Id == phase) {
+                $scope.flatType = $scope.phaseList[i].Phase_UnitType.UnitType_Name;
+            }
+        }
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Proj/BlockDtls/ByPhaseBlocksId",
+            ContentType: 'application/json',
+            data: {
+                "Blocks_Phase_Id": phase,
+                "Blocks_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+            $scope.blockList = data;
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    };
+    $scope.getUnitAllocation = function(obj, formName) {
+        $scope.submit = true;
+        if ($scope[formName].$valid) {
+            var userProjData = [];
+            if (obj.blocks != "") {
+                userProjData.push({
+                    "Blocks_Id": obj.blocks
+                });
+            } else {
+                userProjData.push({
+                    "Phase_Id": obj.phase
+                });
+            }
+
+            angular.element(".loader").show();
+            $http({
+                method: "POST",
+                url: "http://120.138.8.150/pratham/Proj/UnitDtls/ByUnitDtlsBlocksId",
+                ContentType: 'application/json',
+                data: {
+                    "UnitDtls_comp_guid": $cookieStore.get('comp_guid'),
+                    "UnitDtls_Block_Id": obj.blocks
+                }
+            }).success(function(data) {
+                $scope.unitsList = data[0];
+                angular.element(".loader").hide();
+            }).error(function() {
+                angular.element(".loader").hide();
+            });
+        }
+    }
+    $scope.viewUnitCostSheet = function(item) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'unitCostSheet.html',
+            controller: 'unitCostSheet',
+            size: 'lg',
+            backdrop: 'static',
+            resolve: {
+                item: function() {
+                    return item;
+                }
+            }
+        });
+    }
+});
+
+app.controller("unitCostSheet", function($scope, $http, $cookieStore, $state, $stateParams, $filter, $compile, $uibModal, $uibModalInstance, item) {
+    $scope.unitId = item;
+    ($scope.getUnitCostSheetDetails = function() {
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Proj/Blk/UntCstSheet/Gt",
+            ContentType: 'application/json',
+            data: {
+                "UnitDtls_Id": parseInt($scope.unitId),
+                "UnitDtls_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+            console.log(JSON.stringify(data));
+            $scope.unitCostSheetDetail = data;
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    })();
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+});
